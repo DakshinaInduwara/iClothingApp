11cf0823ec1e4fef2c55438bbf352a70a623ddc7
Ashani9696 <ashani9996@gmail.com>
2024-03-26 20:50:25 +0530

    Initial Commit

---
+++ iClothingApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iClothingApp
+//
+//  Created by Ashani Dilanka on 2024-03-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iClothingApp/iClothingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iClothingAppApp.swift
+//  iClothingApp
+//
+//  Created by Ashani Dilanka on 2024-03-26.
+//
+
+import SwiftUI
+
+@main
+struct iClothingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
